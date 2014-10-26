5d1ce209648e9cdfb775ad59b07f8598b34b4446
krujos <jkruck@pivotal.io>
2014-10-25 21:36:26 -0500

    update ruby

--- investigator.rb
+++ investigator.rb
@@ -1,15 +1,14 @@
 require 'sinatra'
 require 'json'
 
-get "/" do 
-  "Avaliable API's <br/> 
-  /env <br/> 
+get '/' do 
+  "Avaliable API's <br/> /env <br/>"
 end
 
-get "/env" do
-  output = "<pre>"
+get '/env' do
+  output = '<pre>'
   output += JSON.pretty_generate(ENV.to_hash)
-  output += "</pre>"
+  output += '</pre>'
   output
 end
 
